05734db3614f25718fcd4e8a9be30a7ac34e8dd2
Nhan Phan <voquy7177@gmail.com>
2025-08-09 18:48:45 +0700

    Add all files

--- split_pdf.py
+++ split_pdf.py
@@ -54,7 +54,7 @@ def split_pdf_in_half(input_file, output_prefix="split"):
 # Cách sử dụng
 if __name__ == "__main__":
     # Thay "your_file.pdf" bằng tên file PDF của bạn
-    input_pdf = "Tieng-Anh-English-Discovery-compressed.pdf"
+    input_pdf = "SGK-Tieng-Anh-7-iLearn-Smart-World-compressed.pdf"
     
     # Kiểm tra file có tồn tại không
     if os.path.exists(input_pdf):
